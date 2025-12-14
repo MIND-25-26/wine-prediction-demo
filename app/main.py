137140f2159c0db9861e87bfb065dbea058f801f
Fredde <welandfr@arcada.fi>
2025-12-14 22:58:37 +0200

    Simplifications

--- app/main.py
+++ app/main.py
@@ -25,11 +25,9 @@ class WineFeatures(BaseModel):
     sulphates: float
     alcohol: float
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 @app.get("/")
 async def root():
-    return FileResponse("static/index.html")
+    return { "msg": "Welcome to the Wine Prediction API!" }
 
 @app.post("/predict")
 def predict_quality(features: WineFeatures):
